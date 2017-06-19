8401dded9235ec8d9a51a17e39eb858bdd24923c
ryujeahong <phnin1106@naver.com>
2017-06-19 19:17:10 +0900

    #3 Swtich Event Programming

--- app/src/main/java/kr/hs/phjin1106e_mirim/audiotest/MainActivity.java
+++ app/src/main/java/kr/hs/phjin1106e_mirim/audiotest/MainActivity.java
@@ -1,13 +1,36 @@
 package kr.hs.phjin1106e_mirim.audiotest;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    Switch switchMusic;
+    MediaPlayer musicPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchMusic=(Switch)findViewById(R.id.switch_music);
+        musicPlayer=(MediaPlayer.create(this, R.raw.music));
+        switchMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switchMusic.isChecked())
+                    musicPlayer.start();
+                else
+                    musicPlayer.pause();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        musicPlayer.stop();
     }
 }
